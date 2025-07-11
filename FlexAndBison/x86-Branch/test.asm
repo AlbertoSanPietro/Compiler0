c5671e1162d070dbacb1fda75a66deb1f4d5ee6a
AlbertoSanPietro <albertoxsp@gmail.com>
2025-07-11 11:25:13 +0200

    added x86 codegen, removed a series of useless files

---
+++ FlexAndBison/x86-Branch/test.asm
@@ -0,0 +1,73 @@
+section .text
+global _start
+_start:
+    push rbp
+    mov rbp, rsp
+    sub rsp, 512
+    mov rax, 2
+    mov [rbp-8], rax
+.L0:
+    mov rax, [rbp-8]
+    push rax
+    mov rax, 10
+    pop rbx
+    cmp rbx, rax
+    setl al
+    movzx rax, al
+    cmp rax, 0
+    je .L1
+    mov rax, [rbp-8]
+    push rax
+    mov rax, 2
+    pop rbx
+    cmp rax, 0
+    je .divzero
+    mov rcx, rax
+    mov rax, rbx
+    cqo
+    idiv rcx
+    mov rax, rdx
+    push rax
+    mov rax, 0
+    pop rbx
+    cmp rbx, rax
+    sete al
+    movzx rax, al
+    cmp rax, 0
+    je .L2
+    mov rax, [rbp-8]
+    push rax
+    mov rax, 1
+    pop rbx
+    add rax, rbx
+    mov [rbp-8], rax
+    jmp .L3
+.L2:
+    mov rax, [rbp-8]
+    push rax
+    mov rax, 2
+    pop rbx
+    add rax, rbx
+    mov [rbp-8], rax
+.L3:
+    jmp .L0
+.L1:
+    mov rax, [rbp-8]
+    mov rdi, rax
+    mov rax, 60
+    syscall
+    mov rdi, 0
+    mov rax, 60
+    syscall
+.divzero:
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, divzero_msg
+    mov rdx, divzero_len
+    syscall
+    mov rdi, 1
+    mov rax, 60
+    syscall
+section .data
+divzero_msg: db "Division by zero!", 10
+divzero_len: equ $ - divzero_msg
